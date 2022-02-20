35a9bf76fba3f2331e03cefcd8d1070a929ee946
Alexandr Drozd <alex.drozd.fd@gmail.com>
2022-02-20 14:32:47 +0300

    feat: add sass, normalize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Intro from "./components/intro/Intro";
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="hello">This is main container (App)</h1>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,12 @@
+import './intro.scss'
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="intro__left">left</div>
+      <div className="intro__right">right</div>
+    </div>
+  );
+};
+
+export default Intro;
