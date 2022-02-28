6566efc7b9b5c9248318e4e91752a1b4c41ca2bf
Alexandr Drozd <alex.drozd.fd@gmail.com>
2022-02-28 20:06:20 +0300

    feat: create toggle component (toggle dark mode)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useContext } from 'react';
+import { ThemeContext } from './context';
 import Intro from './components/intro/Intro';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
+import Toggle from './components/toggle/Toggle';
 import './index.css';
 
 function App() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div className="App">
+    <div 
+      className="App"
+      style={{
+        backgroundColor: darkMode ? '#222' : 'white',
+        color: darkMode && "white",
+      }}
+    >
+      <Toggle />
       <Intro />
       <About />
       <Projects />
